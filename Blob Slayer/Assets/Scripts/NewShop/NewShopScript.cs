5ac4078a6367037f39b83e55fceeff332e9c96b7
Alexandra <98310130+jaalevia@users.noreply.github.com>
2023-06-01 13:47:19 +0300

    Shop price
    Shop price text added

--- Blob Slayer/Assets/Scripts/NewShop/NewShopScript.cs
+++ Blob Slayer/Assets/Scripts/NewShop/NewShopScript.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 
 public class NewShopScript : MonoBehaviour
 {
+    [Header("Текстовые поля для уровня")]
     public TextMeshProUGUI PenText;
-    private int _penInt = 1;
     public TextMeshProUGUI InkText;
-    private int _inkInt = 1;
     public TextMeshProUGUI BlutterText;
+
+    [Header("Текстовые поля для цены")]
+    public TextMeshProUGUI PenPriceText;
+    public TextMeshProUGUI InkPriceText;
+    public TextMeshProUGUI BlutterPriceText;
+
+    [Header("Цена")]
+    public int PenPrice;
+    public int InkPrice;
+    public int BlutterPrice;
+
+    private int _penInt = 1;
+    private int _inkInt = 1;
     private int _blutterInt = 1;
+    
 
     private void Start()
     {
+        PenPriceText.text = "" + PenPrice.ToString();
+        InkPriceText.text = "" + InkPrice.ToString();
+        BlutterPriceText.text = "" + BlutterPrice.ToString();
+
         PenText.text = "" + _penInt.ToString();
         InkText.text = "" + _inkInt.ToString();
         BlutterText.text = "" + _blutterInt.ToString();
