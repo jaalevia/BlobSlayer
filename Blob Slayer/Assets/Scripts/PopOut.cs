6cf849b6ac24f27d59b468d10dca9d10b6858416
Alexandra <98310130+jaalevia@users.noreply.github.com>
2023-06-01 20:18:46 +0300

    Pop Up work

---
+++ Blob Slayer/Assets/Scripts/PopOut.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopOut : MonoBehaviour
+{
+    public List<GameObject> spawnToObject = new List<GameObject>();
+    public static PopOut current;
+    public GameObject prefab;
+    public EnemyScript enemy;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public void CreatePopUp()
+    {
+        int a = Random.Range(0, spawnToObject.Count);
+        enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyScript>();
+        prefab.GetComponent<TextMeshPro>().SetText(enemy._takeDmg.ToString());
+        GameObject popup = Instantiate(prefab, spawnToObject[a].transform.position, Quaternion.identity);
+        popup.transform.SetParent(spawnToObject[a].transform);
+
+        Destroy(popup, 0.2f);
+    }
+}
